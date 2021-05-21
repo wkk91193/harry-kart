cdce71ba1d226634e4a604302c0e4b4e91c852c6
Kumaranath Fernando <wkk91193@yahoo.com>
2021-05-21 10:55:38 +0700

    Adding read data service

---
+++ src/main/java/se/atg/service/harrykart/java/service/ReadRaceData.java
@@ -0,0 +1,7 @@
+package se.atg.service.harrykart.java.service;
+
+import se.atg.service.harrykart.java.model.RaceInfo;
+
+public interface ReadRaceData {
+    public RaceInfo readXmlData(String xmlData);
+}
---
+++ src/main/java/se/atg/service/harrykart/java/service/ReadRaceDataImpl.java
@@ -0,0 +1,11 @@
+package se.atg.service.harrykart.java.service;
+
+import se.atg.service.harrykart.java.model.RaceInfo;
+
+public class ReadRaceDataImpl implements  ReadRaceData {
+
+    @Override
+    public RaceInfo readXmlData(String xmlData) {
+        return null;
+    }
+}
