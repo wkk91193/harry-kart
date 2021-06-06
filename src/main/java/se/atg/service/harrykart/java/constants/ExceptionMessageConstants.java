fb8e0d35934f53f016f7c8eba54a8f4169837e19
Kumaranath Fernando <wkk91193@yahoo.com>
2021-06-06 12:56:05 +0700

    Create class to handle custom exceptions for bad user input

---
+++ src/main/java/se/atg/service/harrykart/java/constants/ExceptionMessageConstants.java
@@ -0,0 +1,12 @@
+package se.atg.service.harrykart.java.constants;
+
+public class ExceptionMessageConstants {
+    private ExceptionMessageConstants(){}
+
+    public static final String NULL_OR_EMPTY_ARGUEMENTS_FOUND ="Null argument found.";
+    public static final String SOMETHING_WENT_WRONG = "Could not process request. Something went wrong.";
+    public static final String JSON_PROCESS_ERROR =  "Something went wrong during serialising result to JSON.";
+    public static final String INVALID_XML=  "INVALID XML. Please verify the correctness of the input.";
+    public static final String INVALID_NUMBER = "Invalid number";
+
+}
---
+++ src/main/java/se/atg/service/harrykart/java/exceptions/UserInputException.java
@@ -0,0 +1,8 @@
+package se.atg.service.harrykart.java.exceptions;
+
+public class UserInputException extends Exception {
+
+    public UserInputException(String message){
+        super(message);
+    }
+}
